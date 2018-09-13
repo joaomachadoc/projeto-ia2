18338d46dd7022591104012855f4126cf9b2dd0d
João Pedro Costa <35932387+joaomachadoc@users.noreply.github.com>
2018-09-13 20:23:31 -0300

    melhorias

--- Sensor_de_temp_com_leds_em_bin.ino
+++ Sensor_de_temp_com_leds_em_bin.ino
@@ -26,168 +26,46 @@ void setup() {
 void loop() {
   double temperatura = (((analogRead(LM35) * 5.0) / 1023.0) * 100.0);  // o analog read vai retornar em volts, as leituras do lm35 e depois (* 100) p trazer a celsius
   Serial.println(temperatura);
-  if(temperatura >= 5 and temperatura <7){
+  if(temperatura >= 5 and temperatura <23){
     digitalWrite(led0, LOW);
-    digitalWrite(led1, HIGH);
+    digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
-    digitalWrite(led3, HIGH);
+    digitalWrite(led3, LOW);
     digitalWrite(led4, LOW);
-  }else{
-      if(temperatura >= 7 and temperatura <9){
-        digitalWrite(led0, LOW);
-        digitalWrite(led1, HIGH);
+  }else{                             
+      if(temperatura >=23 and temperatura <24){
+        digitalWrite(led0, HIGH);
+        digitalWrite(led1, LOW);
         digitalWrite(led2, HIGH);
         digitalWrite(led3, HIGH);
-        digitalWrite(led4, LOW);
+        digitalWrite(led4, HIGH);
       }else{
-          if(temperatura >= 9 and temperatura <11){
+         if(temperatura >= 31 and temperatura <35){
               digitalWrite(led0, HIGH);
-              digitalWrite(led1, LOW);
-              digitalWrite(led2, LOW);
+              digitalWrite(led1, HIGH);
+              digitalWrite(led2, HIGH);
               digitalWrite(led3, HIGH);
-              digitalWrite(led4, LOW);
-          }else{
-              if(temperatura >=11 and temperatura < 13){
-                  digitalWrite(led0, HIGH);
-                  digitalWrite(led1, LOW);
-                  digitalWrite(led2, HIGH);
-                  digitalWrite(led3, HIGH);
-                  digitalWrite(led4, LOW);
-              }else{
-                if(temperatura >= 13 and temperatura <15){
-                  digitalWrite(led0, HIGH);
-                  digitalWrite(led1, HIGH);
-                  digitalWrite(led2, LOW);
-                  digitalWrite(led3, HIGH);
-                  digitalWrite(led4, LOW);
-              }else{
-                if(temperatura >=15 and temperatura <17){
-                    digitalWrite(led0, HIGH);
-                    digitalWrite(led1, HIGH);
-                    digitalWrite(led2, HIGH);
-                    digitalWrite(led3, HIGH);
-                    digitalWrite(led4, LOW);
-                }else{
-                  if(temperatura >= 17 and temperatura < 19){
-                      digitalWrite(led0, HIGH);
-                      digitalWrite(led1, LOW);
-                      digitalWrite(led2, LOW);
-                      digitalWrite(led3, LOW);
-                      digitalWrite(led4, HIGH);
-                  }else{
-                      if(temperatura >=19 and temperatura < 21){
-                          digitalWrite(led0, HIGH);
-                          digitalWrite(led1, LOW);
-                          digitalWrite(led2, LOW);
-                          digitalWrite(led3, HIGH);
-                          digitalWrite(led4, HIGH);
-                      }else{
-                        if(temperatura >21 and temperatura <22){
-                            digitalWrite(led0, HIGH);
-                            digitalWrite(led1, LOW);
-                            digitalWrite(led2, HIGH);
-                            digitalWrite(led3, LOW);
-                            digitalWrite(led4, HIGH);
-                        }else{
-                            if(temperatura >=22 and temperatura <23){
-                                digitalWrite(led0, HIGH);
-                                digitalWrite(led1, LOW);
-                                digitalWrite(led2, HIGH);
-                                digitalWrite(led3, HIGH);
-                                digitalWrite(led4, LOW);
-                            }else{
-                                  if(temperatura >=23 and temperatura <24){
-                                        digitalWrite(led0, HIGH);
-                                        digitalWrite(led1, LOW);
-                                        digitalWrite(led2, HIGH);
-                                        digitalWrite(led3, HIGH);
-                                        digitalWrite(led4, HIGH);
-                                  }else{
-                                      if(temperatura >= 24 and temperatura < 25){
-                                            digitalWrite(led0, HIGH);
-                                            digitalWrite(led1, HIGH);
-                                            digitalWrite(led2, LOW);
-                                            digitalWrite(led3, LOW);
-                                            digitalWrite(led4, LOW);
-                                      }else{
-                                          if(temperatura >= 25 and temperatura <26){
-                                                digitalWrite(led0, HIGH);
-                                                digitalWrite(led1, HIGH);
-                                                digitalWrite(led2, LOW);
-                                                digitalWrite(led3, LOW);
-                                                digitalWrite(led4, HIGH);
-                                          }else{
-                                               if(temperatura >= 26 and temperatura <27){
-                                                    digitalWrite(led0, HIGH);
-                                                    digitalWrite(led1, HIGH);
-                                                    digitalWrite(led2, LOW);
-                                                    digitalWrite(led3, LOW);
-                                                    digitalWrite(led4, LOW);
-                                               }else{
-                                                    if(temperatura >=27 and temperatura <28){
-                                                        digitalWrite(led0, HIGH);
-                                                        digitalWrite(led1, HIGH);
-                                                        digitalWrite(led2, LOW);
-                                                        digitalWrite(led3, HIGH);
-                                                        digitalWrite(led4, HIGH);
-                                                    }else{
-                                                          if(temperatura >=28 and temperatura <30){
-                                                                 digitalWrite(led0, HIGH);
-                                                                 digitalWrite(led1, HIGH);
-                                                                 digitalWrite(led2, HIGH);
-                                                                 digitalWrite(led3, LOW);
-                                                                 digitalWrite(led4, LOW);
-                                                          }else{
-                                                              if(temperatura >=30 and temperatura < 31){
-                                                                  digitalWrite(led0, HIGH);
-                                                                  digitalWrite(led1, HIGH);
-                                                                  digitalWrite(led2, HIGH);
-                                                                  digitalWrite(led3, HIGH);
-                                                                  digitalWrite(led4, LOW);
-                                                              }else{
-                                                                  if(temperatura >= 31 and temperatura <35){
-                                                                      digitalWrite(led0, HIGH);
-                                                                      digitalWrite(led1, HIGH);
-                                                                      digitalWrite(led2, HIGH);
-                                                                      digitalWrite(led3, HIGH);
-                                                                      digitalWrite(led4, HIGH);
-                                                                  }else{
-                                                                        //se for abaixo de 5 graus celsius ou maior que 35
-                                                                        tone (13, 3000, 50);
-                                                                        for(i=0;i<20;i++){
-                                                                            digitalWrite(led0, HIGH);
-                                                                            digitalWrite(led1, HIGH);
-                                                                            digitalWrite(led2, HIGH);
-                                                                            digitalWrite(led3, HIGH);
-                                                                            digitalWrite(led4, HIGH);
-                                                                            delay(100);
-                                                                            digitalWrite(led0, LOW);
-                                                                            digitalWrite(led1, LOW);
-                                                                            digitalWrite(led2, LOW);
-                                                                            digitalWrite(led3, LOW);
-                                                                            digitalWrite(led4, LOW);
-                                                                            delay(100);
-                                                                              
-                                                                        } 
-                                                                  }
-                                                              }
-                                                          }
-                                                    }
-                                               }
-                                          }
-                                      }
-                                  }  
-                            }
-                        }
-                      }
-                  }
-                }
-              }
-          }
+              digitalWrite(led4, HIGH);
+       }else{
+            //se for abaixo de 5 graus celsius ou maior que 35
+            tone (13, 3000, 50);
+            for(i=0;i<20;i++){
+            digitalWrite(led0, HIGH);
+            digitalWrite(led1, HIGH);
+            digitalWrite(led2, HIGH);
+            digitalWrite(led3, HIGH);
+            digitalWrite(led4, HIGH);
+            delay(100);
+            digitalWrite(led0, LOW);
+            digitalWrite(led1, LOW);
+            digitalWrite(led2, LOW);
+            digitalWrite(led3, LOW);
+            digitalWrite(led4, LOW);
+            delay(100);
+            } 
+        }
       }
     }
-  }
-  
   delay(10000);
   //desligando os leds para nao interferir na leitura do lm35, já que ele faz a leitura em volts
   digitalWrite(led0, LOW);
